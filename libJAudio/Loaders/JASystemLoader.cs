6218e74fec36ce5864cc47189ed761aed7b913ad
XAYRGA <code@xayr.ga>
2020-02-24 19:07:17 -0500

    Pikmin 1 BX Support Complete

--- libJAudio/Loaders/JASystemLoader.cs
+++ libJAudio/Loaders/JASystemLoader.cs
@@ -7,7 +7,9 @@ using libJAudio.Types;
 
 // If i was wrong about BX and its isntrument assignments (it assigns them sequentially, but loads into 0 when needed) 
 // then define this.
-// #define BX_XAYR_WRONG  
+
+// #define BX_XAYR_WRONG_INST
+// #define BX_XAYR_WRONG_WSYS
 
 namespace libJAudio.Loaders
 {
@@ -26,6 +28,9 @@ namespace libJAudio.Loaders
                 case JAIInitType.BAA:
                     loadJV2(ref newJA, ref data);
                     break;
+                case JAIInitType.BX:
+                    loadJBX(ref newJA, ref data);
+                    break;
             }
             return newJA;
         }
@@ -33,7 +38,48 @@ namespace libJAudio.Loaders
 
         public static void loadJBX(ref JASystem JAS, ref byte[] data)
         {
+            var ldr = new JA_BXLoader();
+            var sections = ldr.load(ref data);
+            var stm = new System.IO.MemoryStream(data); // Create a stream for it
+            var read = new Be.IO.BeBinaryReader(stm); // Create a reader around the stream
+            var wscount = 0;
+            var inscount = 0;
 
+            for (int i = 0; i < sections.Length; i++) // Loop through the AAF
+            {
+                var current_section = sections[i]; // Select current section
+                switch (current_section.type) // Get the type
+                {
+                    case JAIInitSectionType.IBNK: // Instrument bank
+                        {
+                            stm.Position = current_section.start; // Seek to start
+                            var vx = new JA_IBankLoader_V1(); // Make a loader
+                            var ibnk = vx.loadIBNK(read, current_section.start); // Load it
+#if BX_XAYR_WRONG_INST
+                            JAS.Banks[ibnk.id] = ibnk; //Push into bank array.
+                            inscount++
+#else
+                            JAS.Banks[inscount] = ibnk; //Push into bank array.
+#endif
+                            break;
+                        }
+                    case JAIInitSectionType.WSYS: // Wave System
+                        {
+                            stm.Position = current_section.start; // Seek to start
+                            var vx = new JA_WSYSLoader_V1(); // Create loader
+                            var ws = vx.loadWSYS(read, current_section.start); // load
+#if BX_XAYR_WRONG_WSYS
+                            JAS.WaveBanks[wscount] = ws; // Push to wavebanks
+                            wscount++;
+#else 
+                            JAS.WaveBanks[ws.id] = ws;
+#endif
+                            break;
+                        }
+                    default:
+                        throw new FormatException("libjAudio failed to load bx file, bx file contains more sections than just WSYS/IBNK. Even though we (somehow) loaded the BX properly, we got a section other than just the two that are possible by the format.\n Hah. This is probably the most descriptive error you've ever seen. Which is ironic because the condition to meet it should never be met. I mean, technically its possible. Cosmic ray errors exist, and those things are weird.");
+                }
+            }
         }
 
         /* AAF is safe, we know it's always going to be a V1 instrument format. */
